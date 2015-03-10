75ecb81d3cb67f17b039be2a313f728567106362
Marcos Joia <marcoshoya@gmail.com>
2015-03-10 17:58:41 -0300

    fix db params

--- protected/config/main.php
+++ protected/config/main.php
@@ -5,7 +5,7 @@
 
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
-return array(
+$main = array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'My Web Application',
 
@@ -22,9 +22,7 @@ return array(
 		// uncomment the following to enable the Gii tool
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
-			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
+			'password'=>'cht',
 		),
 		
 	),
@@ -82,3 +80,5 @@ return array(
 		'adminEmail'=>'webmaster@example.com',
 	),
 );
+
+return $main;
