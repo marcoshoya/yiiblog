17336d34529acb79e15f2e95abdcc934a7e9d5df
Marcos Joia <marcoshoya@gmail.com>
2015-03-13 10:31:26 -0300

    identify implementation

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -14,7 +14,7 @@ class UserIdentity extends CUserIdentity
 	 * In practical applications, this should be changed to authenticate
 	 * against some persistent user identity storage (e.g. database).
 	 * @return boolean whether authentication succeeds.
-	 */
+	
 	public function authenticate()
 	{
 		$users=array(
@@ -30,4 +30,31 @@ class UserIdentity extends CUserIdentity
 			$this->errorCode=self::ERROR_NONE;
 		return !$this->errorCode;
 	}
+	 */
+	
+	private $_id;
+	
+	public function authenticate()
+	{
+		$username = strtolower($this->username);
+		$user = User::model()->find('LOWER(username)=?', array($username));
+		
+		if ($user === null) {
+			$this->errorCode=self::ERROR_USERNAME_INVALID;
+		} else if (!$user->validatePassword($this->password)) {
+			$this->errorCode=self::ERROR_PASSWORD_INVALID;
+		} else {
+			$this->_id = $user->id;
+			$this->username = $user->username;
+			$this->errorCode = self::ERROR_NONE;
+		}
+		
+		return $this->errorCode == self::ERROR_NONE;
+	}
+	
+	public function getId()
+	{
+		return $this->_id;
+	}
+	
 }
